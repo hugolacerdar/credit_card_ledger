9e7e1bc129b5bd5a19d56f54bc15d17f72259403
Hugo <hugolacerdar@gmail.com>
2022-02-28 14:01:12 -0300

    chore: add user entity

---
+++ lib/credit_card_ledger/entity/user.ex
@@ -0,0 +1,19 @@
+defmodule CreditCardLedger.Entity.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "users" do
+    field :fullname, :string
+    field :inserted_at, :naive_datetime
+  end
+
+  def cast(params) do
+    truncated_now = NaiveDateTime.utc_now() |> NaiveDateTime.truncate(:second)
+
+    %__MODULE__{inserted_at: truncated_now}
+    |> cast(params, [:fullname])
+    |> validate_required([:fullname])
+  end
+end
